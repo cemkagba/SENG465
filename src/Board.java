6990d62e58e2102c879f83603ff8c185621073eb
sutkucanverdi <c2328013@student.cankaya.edu.tr>
2025-12-28 20:08:16 +0300

    fix Board java initialization and safety checks

--- src/Board.java
+++ src/Board.java
@@ -1,54 +1,46 @@
 public class Board {
-    int size;
-    int[][] grid = new int[size][size];
+    private final int size;
+    private final int[][] grid; // 0: empty, 1: bomb, 2: coin
 
-    Board(int size) {
+    public Board(int size) {
         this.size = size;
         this.grid = new int[size][size];
-    }
-
-    public void setBomb(int x, int y) {
-        grid[x][y] = 1;
-    }
-
-    public void setCoin(int row, int col) {
-        grid[row][col] = 2;
-    }
-
-
-    public boolean isSafe(int r, int c) {
-        if (r < 0 || r >= size || c < 0 || c >= size) {
-            return false;
-        }
-        return grid[r][c] != 1;
-    }
-
-    private boolean isValidCoordinate(int r, int c) {
-        return r >= 0 && r < size && c >= 0 && c < size;
     }
 
     public int getSize() {
         return size;
     }
 
+    public void setBomb(int row, int col) {
+        grid[row][col] = 1;
+    }
+
+    public void setCoin(int row, int col) {
+        grid[row][col] = 2;
+    }
+
+    public boolean isSafe(int row, int col) {
+        return row >= 0 && row < size &&
+               col >= 0 && col < size &&
+               grid[row][col] != 1;
+    }
+
     public void printBoard(int knightR, int knightC) {
-        System.out.println("  a b c d e f g h"); // Column headers
+        System.out.print("  ");
+        for (int c = 0; c < size; c++) {
+            System.out.print((char) ('a' + c) + " ");
+        }
+        System.out.println();
+
         for (int r = 0; r < size; r++) {
-            System.out.print((r + 1) + " "); // Row number
+            System.out.print((r + 1) + " ");
             for (int c = 0; c < size; c++) {
-                if (r == knightR && c == knightC) {
-                    System.out.print("K "); // Knight
-                } else if (grid[r][c] == 1) {
-                    System.out.print("X "); // Bomb
-                } else if (grid[r][c] == 2) {
-                    System.out.print("C "); // Coin
-                } else {
-                    System.out.print(". "); // Empty
-                }
+                if (r == knightR && c == knightC) System.out.print("K ");
+                else if (grid[r][c] == 1) System.out.print("X ");
+                else if (grid[r][c] == 2) System.out.print("C ");
+                else System.out.print(". ");
             }
             System.out.println();
         }
     }
-
-
 }
